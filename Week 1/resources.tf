9f9e55e2b8b2e30c560a34c81f7cb3f991d4e1a2
Giuliana Bentancor <giulianabentancor@gmail.com>
2024-04-24 21:13:58 -0300

    Adding vn tag

--- Week 1/resources.tf
+++ Week 1/resources.tf
@@ -4,4 +4,7 @@ resource "azurerm_virtual_network" "vn_giuli" {
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
   resource_group_name = "GiuliGroup"
+     tags = {
+       source = "backend"
+      }
 }
